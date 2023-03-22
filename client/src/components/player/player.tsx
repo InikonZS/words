1ec9d416888f4bef47f6fd02239446fe5fdf9c4c
Ramitsan <ramitsan@gmail.com>
2023-03-22 18:04:20 +0300

    add player styles and game background

--- client/src/components/player/player.tsx
+++ client/src/components/player/player.tsx
@@ -4,15 +4,16 @@ import '../../style.css';
 import './player.css';
 
 export function Player({playerData, isActive}: {playerData: IPlayerData, isActive:boolean}) {
+    const {name, points, crystals, winWord} = playerData;
     return (
     <div className={`player ${isActive ? 'player--active' : ''}`}>
         <div className="player__name">
-            {playerData.name}
+            {name}
         </div>
         <div className="player__score">
-            <div className="player__points">score: {playerData.points}</div>
-            <div className="player__crystals">crystals: {playerData.crystals}</div>
-            <div className="player__word">word: {playerData.winWord}</div>
+            <div className="player__points">score: {points}</div>
+            <div className="player__crystals">crystals: {crystals}</div>
+            <div className="player__word">word: {winWord}</div>
         </div> 
     </div>    
     )
