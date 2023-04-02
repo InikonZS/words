4e29716f079c05d4e36ac5bf43fb126f53bb640b
Ramitsan <ramitsan@gmail.com>
2023-04-02 19:39:07 +0300

    add user settings popup

--- client/src/lobby/lobby.tsx
+++ client/src/lobby/lobby.tsx
@@ -4,6 +4,7 @@ import { AnimatedList } from '../animatedList';
 import '../style.css';
 import './lobby.css';
 import { useLangContext } from "../context";
+import { UserEditPopup } from '../components/user-edit-popup/user-edit-popup';
 
 interface ILobbyProps {
     socket: Socket;
@@ -12,13 +13,43 @@ interface ILobbyProps {
 }
 
 export function Lobby({ socket, onSingle, onMulti }: ILobbyProps) {
+    const [userEditMode, setUserEditMode] = useState(false);
+    const [ava, setAva] = useState<string | null>(null);
 
     const {setLang, currentLang} = useLangContext();
+
+    const changeAvatar = (str: string) => {
+        setUserEditMode(false)
+        if (!str) {
+          console.log('image is not selected')
+          return
+        }
+        socket.sendState({
+          type: "userAvatar",
+          data: {
+            img: str.slice(str.indexOf(",") + 1)
+          }
+        })
+      }
 
     //const [items, setItems] = useState([]);
     return (
         <div className="lobby">
             <div className="lobby__wrapper">
+            {userEditMode && <UserEditPopup onClose={changeAvatar} />}
+
+            <div className="user-info__wrapper">            
+            <div className="user-info__picture" onClick={() => {
+              setUserEditMode(true)
+            }}>
+              {ava && <img className="user-info__img" src={`${ava}`} width="100" height="100" alt="avatar" />}
+              user block
+            </div>
+            <div className="user-info__info-block">
+              {/* <div className="user-info__username">Hello, <span>{user.userName}</span></div> */}
+            </div>
+          </div>
+
                 {socket ? <div>userName: {socket.name}</div> : <div>connecting...</div>}
                 <button onClick={()=>{
                     setLang();
