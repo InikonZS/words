d700308e327214c8d602f99bfde78d633d7f0206
Ramitsan <ramitsan@gmail.com>
2023-04-04 06:54:43 +0300

    add buttons disabled style

--- client/src/components/hints/hints.tsx
+++ client/src/components/hints/hints.tsx
@@ -13,18 +13,18 @@ export function Hints({crystals, onShuffle, onShowWords, onShowMask} : IHintsPro
 
   return (
     <div className="hints">
-      <h3 className="hints__title">Hints:</h3>
+      <h3 className="hints__title">Available hints:</h3>
       <button className="btn hints__button hints__button--shuffle" disabled={crystals < 1} onClick={() => {
         onShuffle();
       }}>
         shuffle
       </button>
-      <button className="btn hints__button hints__button--show-words" disabled={crystals < 0} onClick={() => {
+      <button className="btn hints__button hints__button--show-words" disabled={crystals < 2} onClick={() => {
         onShowWords();
       }}>
         show words
       </button>
-      <button className="btn hints__button hints__button--show-mask" disabled={crystals < 0} onClick={() => {
+      <button className="btn hints__button hints__button--show-mask" disabled={crystals < 3} onClick={() => {
         onShowMask();
       }}>
         show mask
