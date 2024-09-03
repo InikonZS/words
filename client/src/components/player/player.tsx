0b25ad91ea54f0b7a0c76a82a6489d039c5de651
inikonzs <zhanzs@mail.ru>
2024-09-04 00:22:04 +0200

    change player styles

--- client/src/components/player/player.tsx
+++ client/src/components/player/player.tsx
@@ -7,13 +7,17 @@ export function Player({playerData, isActive}: {playerData: IPlayerData, isActiv
     const {name, points, crystals, winWord} = playerData;
     return (
     <div className={`player ${isActive ? 'player--active' : ''}`}>
+        {!playerData.connected && <div className="player__disconnect">disconnected</div>}
         <div className="player__name">
             {name}
         </div>
         <div className="player__score">
-            <div>connected: {playerData.connected ? 'true' : 'false'}</div>
-            <div className="player__points">score: {points}</div>
-            <div className="player__crystals">crystals: {crystals}</div>
+            <div className="player__points">
+                <span className="player__points-text">score:</span><span className="player__points-value">{points}</span>
+            </div>
+            <div className="player__crystals">
+                {crystals>0?new Array(crystals).fill(0).map(it=><div className="player__crystal"></div>): <div className="player__no-crystals">no crystals</div>}
+            </div>
             <div className="player__word">word: {winWord}</div>
         </div> 
     </div>    
